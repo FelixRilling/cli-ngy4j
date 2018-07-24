0b226f2ef0cb00ae7c1cc109ed318d13ec35c104
Felix Rilling <felix@f-rilling.com>
2018-07-24 20:07:20 +0200

    added input parser matcher

--- src/main/java/com/felixrilling/clingy4j/parser/InputParser.java
+++ src/main/java/com/felixrilling/clingy4j/parser/InputParser.java
@@ -3,21 +3,41 @@ package com.felixrilling.clingy4j.parser;
 import com.felixrilling.clingy4j.command.CommandMap;
 import com.felixrilling.clingy4j.lookup.ILookupResult;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class InputParser {
     private final List<String> legalQuotes;
+    private final Pattern pattern;
 
     public InputParser(List<String> legalQuotes) {
         this.legalQuotes = legalQuotes;
+        this.pattern = generateMatcher();
     }
 
     public ILookupResult parse(CommandMap mapAliased, String input) {
-        //TODO implement
+        List<String> split = Arrays.asList(pattern.split(input));
+
         return null;
     }
 
     public List<String> getLegalQuotes() {
         return legalQuotes;
+    }
+
+    public Pattern getPattern() {
+        return pattern;
+    }
+
+    private Pattern generateMatcher() {
+        List<String> patterns = legalQuotes.stream().map(r -> String.format("%1$s.+?%1$s", r)).collect(Collectors.toList());
+
+        patterns.add("\\S+");
+
+        String regex = String.format("/(%s)/g", String.join("|", patterns));
+
+        return Pattern.compile(regex);
     }
 }
--- src/test/java/com/felixrilling/clingy4j/parser/InputParserTest.java
+++ src/test/java/com/felixrilling/clingy4j/parser/InputParserTest.java
@@ -2,9 +2,22 @@ package com.felixrilling.clingy4j.parser;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class InputParserTest {
 
+    /**
+     * Asserts that {@link InputParser} creates a matcher.
+     */
     @Test
-    public void parse() {
+    public void InputParserCreatesPattern() {
+        List<String> legalQuotes = Arrays.asList("\"", "'");
+        InputParser parser = new InputParser(legalQuotes);
+
+        assertThat(parser.getPattern()).isInstanceOf(Pattern.class);
     }
 }
