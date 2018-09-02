1b2e82ec8c797d5a906c2d5d7f37282a4b73c1b3
Felix Rilling <felix@f-rilling.com>
2018-09-02 10:48:59 +0200

    updated IT

--- src/test/java/com/felixrilling/clingy4j/ClingyIT.java
+++ src/test/java/com/felixrilling/clingy4j/ClingyIT.java
@@ -49,31 +49,31 @@ class ClingyIT {
     }
 
     /**
-     * Asserts that lookup of commands works.
-     */
-    @Test
-    public void commandNormal() {
-        String input1 = "foo 123";
-        LookupResult lookupResult1 = clingy.parse(input1);
-        assertThat(lookupResult1.getType()).isEqualTo(LookupResult.ResultType.SUCCESS);
-        assertThat(lookupResult1.getPathDangling()).containsExactly("123");
-        assertThat(lookupResult1.getPathUsed()).containsExactly("foo");
-        assertThat(((LookupSuccess) lookupResult1).getArgs()).containsEntry("val", "123");
-        assertThat(((LookupSuccess) lookupResult1).getCommand()).isEqualTo(command1);
-    }
-
-    /**
      * Asserts that lookup of commands with args works.
      */
     @Test
+    public void commandNormal() {
+        String input = "foo 123";
+        LookupResult lookupResult = clingy.parse(input);
+        assertThat(lookupResult.getType()).isEqualTo(LookupResult.ResultType.SUCCESS);
+        assertThat(lookupResult.getPathDangling()).containsExactly("123");
+        assertThat(lookupResult.getPathUsed()).containsExactly("foo");
+        assertThat(((LookupSuccess) lookupResult).getArgs()).containsEntry("val", "123");
+        assertThat(((LookupSuccess) lookupResult).getCommand()).isEqualTo(command1);
+    }
+
+    /**
+     * Asserts that lookup of commands without args works.
+     */
+    @Test
     public void commandArgs() {
-        String input2 = "baa 456";
-        LookupResult lookupResult2 = clingy.parse(input2);
-        assertThat(lookupResult2.getType()).isEqualTo(LookupResult.ResultType.SUCCESS);
-        assertThat(lookupResult2.getPathDangling()).containsExactly("456");
-        assertThat(lookupResult2.getPathUsed()).containsExactly("baa");
-        assertThat(((LookupSuccess) lookupResult2).getArgs()).isEmpty();
-        assertThat(((LookupSuccess) lookupResult2).getCommand()).isEqualTo(command2);
+        String input = "baa 456";
+        LookupResult lookupResult = clingy.parse(input);
+        assertThat(lookupResult.getType()).isEqualTo(LookupResult.ResultType.SUCCESS);
+        assertThat(lookupResult.getPathDangling()).containsExactly("456");
+        assertThat(lookupResult.getPathUsed()).containsExactly("baa");
+        assertThat(((LookupSuccess) lookupResult).getArgs()).isEmpty();
+        assertThat(((LookupSuccess) lookupResult).getCommand()).isEqualTo(command2);
     }
 
     /**
@@ -81,13 +81,13 @@ class ClingyIT {
      */
     @Test
     public void commandMissing() {
-        String input3 = "foob";
-        LookupResult lookupResult3 = clingy.parse(input3);
-        assertThat(lookupResult3.getType()).isEqualTo(LookupResult.ResultType.ERROR_NOT_FOUND);
-        assertThat(lookupResult3.getPathDangling()).isEmpty();
-        assertThat(lookupResult3.getPathUsed()).containsExactly(input3);
-        assertThat(((LookupErrorNotFound) lookupResult3).getMissing()).isEqualTo(input3);
-        assertThat(((LookupErrorNotFound) lookupResult3).getSimilar()).contains("foo");
+        String input = "foob";
+        LookupResult lookupResult = clingy.parse(input);
+        assertThat(lookupResult.getType()).isEqualTo(LookupResult.ResultType.ERROR_NOT_FOUND);
+        assertThat(lookupResult.getPathDangling()).isEmpty();
+        assertThat(lookupResult.getPathUsed()).containsExactly(input);
+        assertThat(((LookupErrorNotFound) lookupResult).getMissing()).isEqualTo(input);
+        assertThat(((LookupErrorNotFound) lookupResult).getSimilar()).contains("foo");
     }
 
     /**
@@ -95,11 +95,11 @@ class ClingyIT {
      */
     @Test
     public void commandArgsMissing() {
-        String input4 = "foo";
-        LookupResult lookupResult4 = clingy.parse(input4);
-        assertThat(lookupResult4.getType()).isEqualTo(LookupResult.ResultType.ERROR_MISSING_ARGUMENT);
-        assertThat(lookupResult4.getPathDangling()).isEmpty();
-        assertThat(lookupResult4.getPathUsed()).containsExactly(input4);
-        assertThat(((LookupErrorMissingArgs) lookupResult4).getMissing()).containsExactly(argument1);
+        String input = "foo";
+        LookupResult lookupResult = clingy.parse(input);
+        assertThat(lookupResult.getType()).isEqualTo(LookupResult.ResultType.ERROR_MISSING_ARGUMENT);
+        assertThat(lookupResult.getPathDangling()).isEmpty();
+        assertThat(lookupResult.getPathUsed()).containsExactly(input);
+        assertThat(((LookupErrorMissingArgs) lookupResult).getMissing()).containsExactly(argument1);
     }
 }
